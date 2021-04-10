efbfda3318f3006414412e7a191a27d2d2f346c9
Mark Crossfield <github@nanunanu.co.uk>
2021-04-10 23:13:29 +0100

    CV dynamically built from data file
    Fixes #19

--- _includes/events.md
+++ _includes/events.md
@@ -2,9 +2,9 @@
 
 <script type="text/javascript">
 let eventsContents = 'I do not have any future events scheduled at the moment. Please see the <a href="cv.html">CV page</a> for details of events past.'
-if (events.length > 0) {
+if (futureEvents.length > 0) {
     eventsContents = '<ul>'
-    events.forEach(event => eventsContents+=`<li>${event.slug}</div>`)
+    futureEvents.forEach(event => eventsContents+=`<li>${event.slug}</div>`)
     eventsContents += '</ul>'
 }
 document.getElementById('events-list').innerHTML = eventsContents;
--- events.js
+++ events.js
@@ -3,6 +3,7 @@ test: hello
 ---
 var now = new Date();
 now.setHours(0,0,0,0);
+//https://stackoverflow.com/questions/15397372/javascript-new-date-ordinal-st-nd-rd-th#answer-15397495
 const nth = function(d) {
     if (d > 3 && d < 21) return 'th';
     switch (d % 10) {
@@ -12,35 +13,47 @@ const nth = function(d) {
         default: return "th";
     }
 }
-function formattedDate(startDate, endDate) {
+function formattedDate(startDate, endDate, includeDay = true) {
     let startParts = [{day: 'numeric'}, {month: 'short'}, {year: 'numeric'}].map(d => new Intl.DateTimeFormat('en', d).format(startDate))
-    startParts[0] = `${startParts[0]}${nth(startParts[0])}`
+    startParts[0] = `${startParts[0]}${nth(startParts[0])} `
     let endParts = [{day: 'numeric'}, {month: 'short'}, {year: 'numeric'}].map(d => new Intl.DateTimeFormat('en', d).format(endDate))
-    endParts[0] = `${endParts[0]}${nth(endParts[0])}`
+    endParts[0] = `${endParts[0]}${nth(endParts[0])} `
+    if (includeDay === false) {
+        startParts[0] = ''
+        endParts[0] = ''
+    }
     if (startParts[2] === endParts[2]){
         if (startParts[1] === endParts[1]) {
-            return `${startParts[0]}–${endParts[0]} ${endParts[1]} ${endParts[2]}`
+            if (startParts[0] === endParts[0]) {
+                return `${startParts[0]}${startParts[1]} ${startParts[2]}`
+            }
+            else {
+                return `${startParts[0]}–${endParts[0]}${endParts[1]} ${endParts[2]}`
+            }
         } else {
-            return `${startParts[0]} ${startParts[1]}–${endParts[0]} ${endParts[1]} ${endParts[2]}`
+            return `${startParts[0]}${startParts[1]}–${endParts[0]}${endParts[1]} ${endParts[2]}`
         }
     }
     else {
-        return `${startParts[0]} ${startParts[1]} ${startParts[2]}–${endParts[0]} ${endParts[1]} ${endParts[2]}`
+        return `${startParts[0]}${startParts[1]} ${startParts[2]}–${endParts[0]}${endParts[1]} ${endParts[2]}`
     }
 }
 function eventSlug(event) {
     return `<a href="${event.link}" target="_blank" class="url" rel="nofollow">${event.name}</a>, ${event.location}, ${event.date}, (<a href="${event.socialLink} target="_blank" class="url" rel="nofollow"">social media link</a>)`
 }
-var events = [
+let allEvents = [
 {% for event in site.data.events %}
 {
     name: "{{ event.name }}",
     link: "{{ event.link }}",
     socialLink: "{{ event.social_link }}",
     location: "{{ event.location }}",
-    startDate: Date.parse("{{ event.start_date }}"),
-    endDate: Date.parse("{{ event.end_date }}")
+    startDate: new Date(Date.parse("{{ event.start_date }}")),
+    endDate: new Date(Date.parse("{{ event.end_date }}")),
+    date: formattedDate(Date.parse("{{ event.start_date }}"),Date.parse("{{ event.end_date }}"), true),
+    dateWithoutDay: formattedDate(Date.parse("{{ event.start_date }}"),Date.parse("{{ event.end_date }}"), false)
 },
 {% endfor %}
-].filter(event => event.endDate > now).sort((a, b) => b.start_date - a.start_date).map(event => ({ ...event, date: formattedDate(event.startDate,event.endDate)})).map(event => ({ ...event, slug: eventSlug(event)}))
-var nextEvent = events[0];
+]
+var futureEvents = allEvents.filter(event => event.endDate > now).sort((a, b) => b.startDate - a.startDate).map(event => ({ ...event, slug: eventSlug(event)}))
+var nextEvent = futureEvents[0];
